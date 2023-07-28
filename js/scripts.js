2f2acb2a3a2aaa47ecae7c850e98291d9ec90c71
DWilken02 <Dylan.Wilken06@gmail.com>
2023-07-28 14:47:11 -0700

    add Multiplication and division functions to JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,25 @@ const userNumber3 = parseInt(prompt("Enter a number"));
 const userNumber4 = parseInt(prompt("Enter another number"));
 
 window.alert(subtract(userNumber3, userNumber4));
+
+function multiply(number1, number2) {
+  console.log("multiply function number1: ",number1);
+  console.log("multiply function number2: ",number2);
+  return number1 * number2;
+}
+
+const userNumber5 = parseInt(prompt("Enter a number"));
+const userNumber6 = parseInt(prompt("Enter another number"));
+
+window.alert(multiply(userNumber5, userNumber6));
+
+function divide(number1, number2) {
+  console.log("divide function number1: ",number1);
+  console.log("divide function number2: ",number2);
+  return number1 / number2;
+}
+
+const userNumber7 = parseInt(prompt("Enter a number"));
+const userNumber8 = parseInt(prompt("Enter another number"));
+
+window.alert(divide(userNumber7, userNumber8));
