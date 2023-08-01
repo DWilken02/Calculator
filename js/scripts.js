1d4ee2824822981607909b90b7ec458a8fa93772
DWilken02 <Dylan.Wilken06@gmail.com>
2023-08-01 14:35:31 -0700

    WIP add css file and Update JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,26 @@
 function add(number1, number2) {
-  console.log("add function number1: ",number1);
-  console.log("add function number2: ",number2);
   return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
 }
 
 const userNumber1 = parseInt(prompt("Enter a number"));
 const userNumber2 = parseInt(prompt("Enter another number"));
 
 const sum = add(userNumber1, userNumber2);
+const difference = subtract(userNumber1, userNumber2);
+const product = multiply(userNumber1, userNumber2);
+const quotient = divide(userNumber1, userNumber2);
 
-window.alert("Your sum is: " + sum + "!!");
 
-function subtract(number1, number2) {
-  console.log("subtract function number1: ",number1);
-  console.log("subtract function number2: ",number2);
-  return number1 - number2;
-}
-
-const userNumber3 = parseInt(prompt("Enter a number"));
-const userNumber4 = parseInt(prompt("Enter another number"));
-
-const difference = subtract(userNumber3, userNumber4);
-
-window.alert("The difference is: " + difference + "!!");
-
-function multiply(number1, number2) {
-  console.log("multiply function number1: ",number1);
-  console.log("multiply function number2: ",number2);
-  return number1 * number2;
-}
-
-const userNumber5 = parseInt(prompt("Enter a number"));
-const userNumber6 = parseInt(prompt("Enter another number"));
-
-const product = multiply(userNumber5, userNumber6);
-
-window.alert("The product is: " + product + "!!");
-
-function divide(number1, number2) {
-  console.log("divide function number1: ",number1);
-  console.log("divide function number2: ",number2);
-  return number1 / number2;
-}
-
-const userNumber7 = parseInt(prompt("Enter a number"));
-const userNumber8 = parseInt(prompt("Enter another number"));
-
-const quotient = divide(userNumber7, userNumber8);
-
-window.alert("The quotient is: " + quotient + "!!");
+window.alert(userNumber1 + "+" + userNumber2 + "=" + sum + ", "+ userNumber1 + "-" + userNumber2 + "=" + difference + ", " + userNumber1 + "*" + userNumber2 + "=" + product + ", and "+ userNumber1 + "/" + userNumber2 + "=" + quotient);
