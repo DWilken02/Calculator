dd2ca41c6f90f5acb1ba4361525edbc0f72574ed
DWilken02 <Dylan.Wilken06@gmail.com>
2023-09-15 14:16:53 -0700

    make changes to html and JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
-function add(number1, number2) {
-  return number1 + number2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(number1, number2) {
-  return number1 / number2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-const userNumber1 = parseInt(prompt("Enter a number"));
-const userNumber2 = parseInt(prompt("Enter another number"));
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
 
-const sum = add(userNumber1, userNumber2);
-const difference = subtract(userNumber1, userNumber2);
-const product = multiply(userNumber1, userNumber2);
-const quotient = divide(userNumber1, userNumber2);
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
 
+  document.getElementById("output").innerText = result;
+}
 
-window.alert(userNumber1 + "+" + userNumber2 + "=" + sum + ", "+ userNumber1 + "-" + userNumber2 + "=" + difference + ", " + userNumber1 + "*" + userNumber2 + "=" + product + ", and "+ userNumber1 + "/" + userNumber2 + "=" + quotient);
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
